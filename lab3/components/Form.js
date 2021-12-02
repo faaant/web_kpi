6beacde26bfb11a84d93d93fb07489927dc42138
faaant <illiaeducation@gmail.com>
2021-12-02 19:05:42 +0200

    add form.elements

--- lab3/components/Form.js
+++ lab3/components/Form.js
@@ -46,8 +46,8 @@ export default function Form({ close }) {
     e.preventDefault();
     setSpinnerVisibility(true);
     const bodyToSend = {
-      theme: e.target.Theme.value,
-      post: e.target.Post.value,
+      theme: e.target.elements.Theme.value,
+      post: e.target.elements.Post.value,
     };
     fetch("/api/server", {
       method: "POST",
