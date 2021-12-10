afbaef55bb64e0a774da33139d1bcbb5f3096fb2
faaant <illiaeducation@gmail.com>
2021-12-11 00:55:49 +0200

    delete unnecesarry trycatch

--- lab3/components/Form.js
+++ lab3/components/Form.js
@@ -56,11 +56,7 @@ export default function Form({ close }) {
       body: JSON.stringify(bodyToSend),
     })
       .then((resp) => {
-        try {
-          return resp.json();
-        } catch (error) {
-          return error;
-        }
+        return resp.json();
       })
       .then((data) => {
         if (data.meta.data?.message) {
