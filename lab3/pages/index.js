f5ab307ad3766c618ad49e19c89b8d3931f45fb3
faaant <illiaeducation@gmail.com>
2022-01-08 17:29:22 +0200

    add viewport

--- lab3/pages/index.js
+++ lab3/pages/index.js
@@ -68,6 +68,10 @@ export default function Home() {
         <Head>
           <title>Blog</title>
           <link rel="icon" href="/blog-icon.svg" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
         </Head>
 
         <header>
